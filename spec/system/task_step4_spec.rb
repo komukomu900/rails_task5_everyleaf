6ff81e71991889b2493ddc0752d5bcb322fe9b7f
komukomu900 <z9l2c26@gmail.com>
2021-01-23 00:54:02 +0900

    テスト項目の追加

--- spec/system/task_step4_spec.rb
+++ spec/system/task_step4_spec.rb
@@ -1,12 +1,63 @@
 require 'rails_helper'
 describe 'ユーザ管理機能', type: :system do
-  describe '機能' do
-    before do
-      FactoryBot.create(:user1)
-      FactoryBot.create(:user2)
-      FactoryBot.create(:user3)
-      FactoryBot.create(:user4)
+  before do
+    @user1 = FactoryBot.create(:user1)
+    FactoryBot.create(:user2)
+    FactoryBot.create(:user3)
+    FactoryBot.create(:user4)
+  end
+  describe 'ユーザー登録のテスト' do
+    context 'ユーザーを新規作成した場合' do
+      it '作成後マイページが表示される' do
+        visit new_user_path
+        fill_in 'user_name', with: 'yuyu'
+        fill_in 'user_email', with: 'user@yuyu.com'
+        fill_in 'user_password', with: 'hogegege'
+        fill_in 'user_password_confirmation', with: 'hogegege'
+        click_button '作成'
+        expect(page).to have_content 'yuyu'
+        expect(page).to have_content 'user@yuyu.com'
+      end
     end
+  end
+  describe 'セッション機能のテスト' do
+    context 'ログイン時の操作検証' do
+      it 'ログインせずにタスク一覧に入ろうとした場合ログイン画面が表示される' do
+        visit new_session_path
+        expect(page).to have_button 'ログイン'
+      end
+      it 'ログインができる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'test4@test.com'
+        fill_in 'session_password', with: 'test'
+        click_button 'ログイン'
+        expect(page).to have_content 'test4'
+        expect(page).to have_content 'test4@test.com'
+      end
+    end
+    context 'ログイン後の操作検証' do
+      before do
+        visit new_session_path
+        fill_in 'session_email', with: 'test4@test.com'
+        fill_in 'session_password', with: 'test'
+        click_button 'ログイン'
+      end
+      it 'ログイン後マイページが表示される' do
+        expect(page).to have_content 'test4'
+        expect(page).to have_content 'test4@test.com'
+        expect(page).to have_link 'ユーザ編集'
+      end
+      it '他人の詳細画面にアクセスすると参照できずタスク一覧が表示される' do
+        visit user_path(@user1)
+        expect(page).to have_content 'この操作はできません'
+      end
+      it 'ログアウトができる' do
+        click_link 'ログアウト'
+        expect(page).to have_content 'ログアウトしました'
+      end
+    end
+  end
+  describe '機能' do
     context '管理ユーザーでの操作' do
       before do
         visit new_session_path
